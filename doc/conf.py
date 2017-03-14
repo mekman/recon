e3586ac6a034efeb0102a9c4cb6d95cb977ef1ef
Matthias Ekman <matthias.ekman@gmail.com>
2017-03-14 17:17:09 +0100

    rename shablone -> recon

--- doc/conf.py
+++ doc/conf.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 #
-# shablona documentation build configuration file, created by
+# recon documentation build configuration file, created by
 # sphinx-quickstart on Tue Apr 14 10:29:06 2015.
 #
 # This file is execfile()d with the current directory set to its
@@ -17,8 +17,8 @@ import sys
 import os
 
 # General information about the project.
-project = 'shablona'
-copyright = '2015, Ariel Rokem'
+project = 'recon'
+copyright = '2017, Matthias Ekman'
 
 currentdir = os.path.abspath(os.path.dirname(__file__))
 ver_file = os.path.join(currentdir, '..', project, 'version.py')
@@ -211,7 +211,7 @@ html_domain_indices = False
 #html_file_suffix = None
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'shablonadoc'
+htmlhelp_basename = 'recondoc'
 
 
 # -- Options for LaTeX output ---------------------------------------------
@@ -231,8 +231,8 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-  ('index', 'shablona.tex', 'shablona Documentation',
-   'Ariel Rokem', 'manual'),
+  ('index', 'recon.tex', 'recon Documentation',
+   'Matthias Ekman', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
@@ -261,8 +261,8 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    ('index', 'shablona', 'shablona Documentation',
-     ['Ariel Rokem'], 1)
+    ('index', 'recon', 'recon Documentation',
+     ['Matthias Ekman'], 1)
 ]
 
 # If true, show URL addresses after external links.
@@ -275,8 +275,8 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-  ('index', 'shablona', 'shablona Documentation',
-   'Ariel Rokem', 'shablona', 'One line description of project.',
+  ('index', 'recon', 'recon Documentation',
+   'Matthias Ekman', 'recon', 'One line description of project.',
    'Miscellaneous'),
 ]
 
--- doc/index.rst
+++ doc/index.rst
@@ -1,14 +1,15 @@
-.. shablona documentation master file, created by sphinx-quickstart on Tue Apr 14 10:29:06 2015. You can adapt this file completely to your liking, but it should at least contain the root `toctree` directive.
+.. recon documentation master file, created by sphinx-quickstart on Tue Apr 14 10:29:06 2015. You can adapt this file completely to your liking, but it should at least contain the root `toctree` directive.
 
-Welcome to shablona's documentation!
+Welcome to recon's documentation!
 ====================================
 
-`Shablona` is a template for a small scientific Python project. 
+`recon` is a python module for the stimulus **recon**struction from neuroimaging
+data.
 
-To see how to use it, please refer to the `README file 
-<https://github.com/uwescience/shablona/blob/master/README.md>`_ in the Github repository.
+To see how to use it, please refer to the `README file
+<https://github.com/mekman/recon/blob/master/README.md>`_ in the Github repository.
 
-This is an example of documentation of the software, using sphinx_. 
+This is an example of documentation of the software, using sphinx_.
 
 .. _sphinx: http://sphinx-doc.org/
 
@@ -27,4 +28,3 @@ Indices and tables
 * :ref:`genindex`
 * :ref:`modindex`
 * :ref:`search`
-
